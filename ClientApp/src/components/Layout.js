6d88bea9224cd304a9c4ab11b1f8a8acec6abbb0
aratcha <aratchamm@gmail.com>
2023-04-24 22:13:19 +0700

    add

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -9,7 +9,7 @@ export class Layout extends Component {
     return (
       <div>
         <NavMenu />
-        <Container tag="main">
+        <Container fluid tag="main">
           {this.props.children}
         </Container>
       </div>
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -24,7 +24,7 @@ export class NavMenu extends Component {
   render() {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container-fuild light>
           <NavbarBrand tag={Link} to="/">mvc_react</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
@@ -36,7 +36,11 @@ export class NavMenu extends Component {
                 <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+              <button type="button" class="icon-button">
+              <span class="material-icons">notifications</span>
+              <span class="icon-button__badge">2</span>
+              </button>
+
               </NavItem>
             </ul>
           </Collapse>
