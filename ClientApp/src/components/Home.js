9af3dd8fa6120d814bebaebc6a84d0be8798dd15
aratcha <aratchamm@gmail.com>
2023-04-25 00:20:04 +0700

    add

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 
 export class Home extends Component {
@@ -12,23 +14,50 @@ export class Home extends Component {
         <div class="row">
           <div class="col text-center" id="navLeft">
             <div class="profile">
-              <img id="profileIMG" src="https://sv1.picz.in.th/images/2023/04/22/yz2U39.jpg"></img>
+              <img id="profileIMG" src="https://i.pinimg.com/564x/f7/2d/e3/f72de35fb3f8690080cfdad5a54a0662.jpg"></img>
+              <br /><br />
               <div class="Info">
                 <p>
-                  <span id="editInfo"><span class="material-symbols-outlined">edit</span>edit</span><br />
-                  <span id="nameInfo">Name Name</span><br />
-                  <span id="telInfo">088-888-8888</span><br />
+                  <span id="editInfo"><span class="material-symbols-outlined">edit</span> edit</span><br />
+                  <span id="nameInfo"><b>Name Name</b></span><br />
+                  <span id="telInfo">088-888-8888</span><br /><br />
                 </p>
-              </div>
+              
+              <br />
+              <ul className="navbar-nav flex-grow">
+              <NavItem>
+                <NavLink tag={Link} id="HomeButton" className="text-dark" to="/"><img id="HomeIconButton" src="https://sv1.picz.in.th/images/2023/04/24/y3dDQv.png"></img><b>Home</b></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} id="StatusButton" className="text-dark" to="/counter"><img id="StatusIconButton" src="https://sv1.picz.in.th/images/2023/04/25/y3S5mJ.png"></img><b>Status</b></NavLink>
+              </NavItem>
+            </ul>
+            </div>
+
+
             </div>
           </div>
           <div class="col-8 text-center" id="navCenter">
-          <div id="compo" class="container">
-          
-          <img class="picCom" src="https://www.workitdaily.com/media-library/man-updates-his-linkedin-profile.jpg?id=27003617"></img>
-          <p>fsafsa</p>
-          <img class="plus" src="https://sv1.picz.in.th/images/2023/04/24/yfGoil.png"></img>
-          </div>
+            <div id="compo" class="container">
+
+              <img class="picCom" src="https://www.workitdaily.com/media-library/man-updates-his-linkedin-profile.jpg?id=27003617"></img>
+              <p>fsafsa</p>
+              <div class="container">
+                <div class="row">
+                  <div class="col-8">
+                    {/* เพิ่มโปรไฟล์คนมาถ้ามีคนสั่งเพิ่ม */}
+                    <img class="photoCol" src="https://i.pinimg.com/564x/f7/2d/e3/f72de35fb3f8690080cfdad5a54a0662.jpg"></img>
+                    <img class="photoCol" src="https://i.pinimg.com/564x/f7/2d/e3/f72de35fb3f8690080cfdad5a54a0662.jpg"></img>
+                    {/* ถ้าคนสั่งเพิ่มมากกว่า 3 คน */}
+                    <img class="photoCol" src="https://sv1.picz.in.th/images/2023/04/24/ykJ4XJ.png"></img>
+                  </div>
+                  <div class="col-sm">
+                  <img class="plus" src="https://sv1.picz.in.th/images/2023/04/24/yfGoil.png"></img>
+                  </div>
+                </div>
+              </div>
+
+            </div>
           </div>
           <div class="col text-center" id="navRight">
             text3
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -25,16 +25,11 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container-fuild light>
-          <NavbarBrand tag={Link} to="/">mvc_react</NavbarBrand>
+          <NavbarBrand tag={Link} to="/"><img src="https://sv1.picz.in.th/images/2023/04/08/mlIkAa.png" alt="logo.png" border="0"
+                        width="100px" /></NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
               <NavItem>
               <button type="button" class="icon-button">
               <span class="material-icons">notifications</span>
